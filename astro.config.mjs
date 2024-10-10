a35032ab02a9b6e9d4982f0546e8df5ccf83503d
Zander Lewis <zander@zanderlewis.dev>
2024-10-09 21:22:46 -0400

    Re-add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://zanderlewis.dev',
+    integrations: [sitemap()],
 });
