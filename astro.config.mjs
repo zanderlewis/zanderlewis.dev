1664f4b4b0f4410a3d002567df5b0ec9603cd97e
Zander <zander@zanderlewis.dev>
2025-02-18 18:35:08 -0500

    Upgrade TailwindCSS to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  integrations: [tailwind()]
+  vite: {
+    plugins: [tailwindcss()],
+  }
 });
