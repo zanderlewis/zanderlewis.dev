67013754a67048c6f46559294a5c31e6a8c8689c
Zander Lewis <zander@zanderlewis.dev>
2025-04-17 18:15:19 -0400

    Multiple Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://zanderlewis.dev',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   }
