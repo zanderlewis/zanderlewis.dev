a1f3b05bcfd760578ea7d2b81b731eb8d0407fc7
Zander Lewis <zander@zanderlewis.dev>
2026-03-28 22:33:38 -0400

    Gigalixir: relax Elixir requirement to 1.18 for build

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Website.MixProject do
     [
       app: :website,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
